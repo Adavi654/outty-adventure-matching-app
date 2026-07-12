5baffa02bb86aa9e56c7c0e557320679b3bf4ee1
JoePent <joepent18@gmail.com>
2026-07-12 01:47:00 -0400

    feat(login) - Add login page and router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css'
-import ProfileForm from './components/ProfileForm'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import ProfileForm from './components/ProfileForm';
 
 function App() {
   return (
-    <div>
-      <h1>Create Profile</h1>
-      <p>Welcome to Outty! Complete your profile below.</p>
-      <ProfileForm />
-    </div>
-  )
+    <Router>
+      <Routes>
+
+        <Route path="/login" element={<LoginPage />} />
+        
+        <Route path="/profile" element={<ProfileForm />} />
+
+        <Route path="/" element={<Navigate to="/login" />} />
+        
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
